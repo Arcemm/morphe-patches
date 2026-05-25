c36424f943eb147c68c088ac3cffcd2ee2160c02
Arcem. <62380870+Arcemm@users.noreply.github.com>
2026-05-26 00:55:18 +0200

    Update Fingerprints.kt
    test to enable MAX 1

--- patches/src/main/kotlin/hoodles/morphe/patches/duolingo/premium/Fingerprints.kt
+++ patches/src/main/kotlin/hoodles/morphe/patches/duolingo/premium/Fingerprints.kt
@@ -2,9 +2,9 @@ package hoodles.morphe.patches.duolingo.premium
 
 import app.morphe.patcher.Fingerprint
 
-// Matches LoggedInState.toString()
+// Cible la nouvelle classe qui remplace LoggedInState (hy/c1)
 object LoggedInStateFingerprint : Fingerprint(
-    strings = listOf("LoggedIn(user=", ")")
+    strings = listOf("Lhy/c1")
 )
 
 // Matches User.toString()
@@ -15,17 +15,18 @@ object UserFingerprint : Fingerprint(
 // Some method that has to do with subscription trials
 object UserIsPaidFieldUsageFingerprint : Fingerprint(
     parameters = listOf("L", "L"),
-    returnType = "Z",
-    strings = listOf("user", "onboardingState")
+                                                     returnType = "Z",
+                                                     strings = listOf("user", "onboardingState")
 )
 
 // Some method that has to do with checking if MAX is enabled
+// (on le garde même s’il n’est plus utilisé pour hasGold, au cas où on en ait besoin plus tard)
 object UserHasGoldFieldUsageFingerprint : Fingerprint(
     parameters = listOf("L", "L", "L"),
-    returnType = "L",
-    strings = listOf(
-        "maxFeaturesEnabled",
-        "isEmaEnabledInCourse",
-        "user"
-    )
+                                                      returnType = "L",
+                                                      strings = listOf(
+                                                          "maxFeaturesEnabled",
+                                                          "isEmaEnabledInCourse",
+                                                          "user"
+                                                      )
 )
